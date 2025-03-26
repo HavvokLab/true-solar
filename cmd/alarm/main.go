4f043fab8af27f05a3060729bcb0a48f0d3f3def
0xanonydxck <tk.thanatorn@gmail.com>
2025-03-26 19:56:32 +0700

    feat: implement comprehensive alarm handling for multiple solar monitoring clients including Growatt, Huawei, Kstar, and Solarman with Redis integration

---
+++ cmd/alarm/main.go
@@ -0,0 +1,178 @@
+package main
+
+import (
+	"time"
+
+	"github.com/HavvokLab/true-solar/alarm"
+	"github.com/HavvokLab/true-solar/config"
+	"github.com/HavvokLab/true-solar/infra"
+	"github.com/HavvokLab/true-solar/pkg/logger"
+	"github.com/HavvokLab/true-solar/repo"
+	"github.com/rs/zerolog/log"
+	"github.com/sourcegraph/conc"
+)
+
+func init() {
+	logger.Init("alarm.log")
+	loc, _ := time.LoadLocation("Asia/Bangkok")
+	time.Local = loc
+}
+
+func main() {
+	wg := conc.NewWaitGroup()
+	wg.Go(growatt)
+	wg.Go(kstar)
+	wg.Go(huawei)
+	wg.Go(solarman)
+	if err := wg.WaitAndRecover(); err != nil {
+		log.Panic().Any("recover", err.Value).Msg("error wait group")
+	}
+}
+
+func growatt() {
+	credRepo := repo.NewGrowattCredentialRepo(infra.GormDB)
+	credentials, err := credRepo.FindAll()
+	if err != nil {
+		log.Panic().Err(err).Msg("error find all credentials")
+	}
+
+	snmp, err := infra.NewSnmpOrchestrator(infra.TrapTypeGrowattAlarm, config.GetConfig().SnmpList)
+	if err != nil {
+		log.Panic().Err(err).Msg("error create snmp orchestrator")
+	}
+
+	rdb, err := infra.NewRedis()
+	if err != nil {
+		log.Panic().Err(err).Msg("error create redis")
+	}
+
+	wg := conc.NewWaitGroup()
+	for _, credential := range credentials {
+		cred := credential
+		wg.Go(func() {
+			serv := alarm.NewGrowattAlarm(
+				repo.NewSolarRepo(infra.ElasticClient),
+				snmp,
+				rdb,
+			)
+
+			serv.Run(&cred)
+		})
+	}
+
+	if r := wg.WaitAndRecover(); r != nil {
+		log.Panic().Any("recover", r.Value).Msg("error wait group")
+	}
+}
+
+func huawei() {
+	credRepo := repo.NewHuaweiCredentialRepo(infra.GormDB)
+	credentials, err := credRepo.FindAll()
+	if err != nil {
+		log.Panic().Err(err).Msg("error find all credentials")
+	}
+
+	snmp, err := infra.NewSnmpOrchestrator(infra.TrapTypeHuaweiAlarm, config.GetConfig().SnmpList)
+	if err != nil {
+		log.Panic().Err(err).Msg("error create snmp orchestrator")
+	}
+
+	rdb, err := infra.NewRedis()
+	if err != nil {
+		log.Panic().Err(err).Msg("error create redis")
+	}
+
+	wg := conc.NewWaitGroup()
+	for _, credential := range credentials {
+		cred := credential
+		if cred.Version != 1 {
+			continue
+		}
+
+		wg.Go(func() {
+			serv := alarm.NewHuaweiAlarm(
+				repo.NewSolarRepo(infra.ElasticClient),
+				snmp,
+				rdb,
+			)
+
+			serv.Run(&cred)
+		})
+	}
+
+	if r := wg.WaitAndRecover(); r != nil {
+		log.Panic().Any("recover", r.Value).Msg("error wait group")
+	}
+}
+
+func kstar() {
+	credRepo := repo.NewKStarCredentialRepo(infra.GormDB)
+	credentials, err := credRepo.FindAll()
+	if err != nil {
+		log.Panic().Err(err).Msg("error find all credentials")
+	}
+
+	snmp, err := infra.NewSnmpOrchestrator(infra.TrapTypeKstarAlarm, config.GetConfig().SnmpList)
+	if err != nil {
+		log.Panic().Err(err).Msg("error create snmp orchestrator")
+	}
+
+	rdb, err := infra.NewRedis()
+	if err != nil {
+		log.Panic().Err(err).Msg("error create redis")
+	}
+
+	wg := conc.NewWaitGroup()
+	for _, credential := range credentials {
+		cred := credential
+		wg.Go(func() {
+			serv := alarm.NewKstarAlarm(
+				repo.NewSolarRepo(infra.ElasticClient),
+				snmp,
+				rdb,
+			)
+
+			serv.Run(&cred)
+		})
+	}
+
+	if r := wg.WaitAndRecover(); r != nil {
+		log.Panic().Any("recover", r.Value).Msg("error wait group")
+	}
+}
+
+func solarman() {
+	credRepo := repo.NewSolarmanCredentialRepo(infra.GormDB)
+	credentials, err := credRepo.FindAll()
+	if err != nil {
+		log.Panic().Err(err).Msg("error find all credentials")
+	}
+
+	snmp, err := infra.NewSnmpOrchestrator(infra.TrapTypeSolarmanAlarm, config.GetConfig().SnmpList)
+	if err != nil {
+		log.Panic().Err(err).Msg("error create snmp orchestrator")
+	}
+
+	rdb, err := infra.NewRedis()
+	if err != nil {
+		log.Panic().Err(err).Msg("error create redis")
+	}
+
+	wg := conc.NewWaitGroup()
+	for _, credential := range credentials {
+		cred := credential
+		wg.Go(func() {
+			serv := alarm.NewSolarmanAlarm(
+				repo.NewSolarRepo(infra.ElasticClient),
+				snmp,
+				rdb,
+			)
+
+			serv.Run(&cred)
+		})
+	}
+
+	if r := wg.WaitAndRecover(); r != nil {
+		log.Panic().Any("recover", r.Value).Msg("error wait group")
+	}
+}
