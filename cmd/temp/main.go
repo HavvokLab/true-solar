663a4bdd713a213038e9cde2d6768a66670b15d6
0xanonydxck <tk.thanatorn@gmail.com>
2025-04-19 18:34:44 +0700

    feat: add CSV processing and document generation for temperature data with Elasticsearch integration

---
+++ cmd/temp/main.go
@@ -0,0 +1,129 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"time"
+
+	"github.com/HavvokLab/true-solar/infra"
+	"github.com/HavvokLab/true-solar/model"
+	"github.com/gocarina/gocsv"
+	"github.com/olivere/elastic/v7"
+	"github.com/rs/zerolog/log"
+)
+
+type Temp struct {
+	Id          string `csv:"id"`
+	Month       string `csv:"month"`
+	Vendor      string `csv:"vendor"`
+	Area        string `csv:"area"`
+	SiteName    string `csv:"site_name"`
+	Owner       string `csv:"owner"`
+	Capacity    string `csv:"capacity"`
+	NewCapacity string `csv:"new_capacity"`
+}
+
+func (t *Temp) Index() string {
+	return fmt.Sprintf("%v-%v.*", model.SolarIndex, t.Date().Format("2006.01"))
+}
+
+func (t *Temp) Date() time.Time {
+	date, err := time.Parse("1/2/2006", t.Month)
+	if err != nil {
+		log.Warn().Err(err).Str("month", t.Month).Msg("failed to parse date")
+		return time.Time{}
+	}
+	return date
+}
+
+func (t *Temp) CapacityFloat() float64 {
+	capacity, err := strconv.ParseFloat(t.Capacity, 64)
+	if err != nil {
+		return 0
+	}
+	return capacity
+}
+
+func (t *Temp) NewCapacityFloat() float64 {
+	newCapacity, err := strconv.ParseFloat(t.NewCapacity, 64)
+	if err != nil {
+		return 0
+	}
+	return newCapacity
+}
+
+func main() {
+	file, err := os.Open("temp.csv")
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to open file")
+	}
+	defer file.Close()
+
+	var temps []Temp
+	if err := gocsv.UnmarshalFile(file, &temps); err != nil {
+		log.Fatal().Err(err).Msg("failed to unmarshal CSV")
+	}
+
+	bulkDocuments := make([]model.BulkDocument, 0)
+	for _, temp := range temps {
+		ids := findDocumentId(context.Background(), &temp)
+		date := temp.Date()
+		for _, id := range ids {
+			doc := model.BulkDocument{
+				Date:       &date,
+				BulkType:   model.BulkUpdate,
+				DocumentId: id,
+				Document: map[string]any{
+					"installed_capacity": temp.NewCapacityFloat(),
+				},
+			}
+			bulkDocuments = append(bulkDocuments, doc)
+		}
+	}
+
+	file, err = os.Create("documents.json")
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to create file")
+	}
+	defer file.Close()
+
+	encoder := json.NewEncoder(file)
+	encoder.SetIndent("", "  ")
+	if err := encoder.Encode(bulkDocuments); err != nil {
+		log.Fatal().Err(err).Msg("failed to encode documents")
+	}
+
+	log.Info().Int("count", len(bulkDocuments)).Msg("✅ documents written to file")
+}
+
+func findDocumentId(ctx context.Context, temp *Temp) []string {
+	query := elastic.NewBoolQuery().
+		Must(
+			elastic.NewTermQuery("data_type.keyword", model.DataTypePlant),
+			elastic.NewTermQuery("vendor_type.keyword", temp.Vendor),
+			elastic.NewTermQuery("name.keyword", temp.SiteName),
+			elastic.NewTermQuery("id.keyword", temp.Id),
+		)
+
+	client := infra.ElasticClient
+	searchResult, err := client.Search().
+		Index(temp.Index()).
+		Query(query).
+		Size(1000).
+		FetchSourceContext(elastic.NewFetchSourceContext(true).Include("_id")).
+		Do(ctx)
+
+	if err != nil {
+		log.Fatal().Err(err).Msg("failed to search for document")
+	}
+
+	ids := make([]string, 0, len(searchResult.Hits.Hits))
+	for _, hit := range searchResult.Hits.Hits {
+		ids = append(ids, hit.Id)
+	}
+
+	return ids
+}
